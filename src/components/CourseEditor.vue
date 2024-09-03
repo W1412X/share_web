728a80b66709f6ea95b8786d0c0b978cf0c20761
W1412X <tor108@outlook.com>
2024-09-04 00:50:24 +0800

    add method

--- src/components/CourseEditor.vue
+++ src/components/CourseEditor.vue
@@ -1,140 +1,165 @@
 <template>
-    <v-card style="width: 750px; padding: 10px">
-      <div style="display: flex; flex-direction: column">
-        <div style="display: flex; flex-direction: row">
-          <span style="font-size: 18px; font-weight: 600"> 编辑新课程 </span>
-        </div>
-        <div style="display: flex; flex-direction: row; margin-top: 10px">
-          <v-textarea
-            label="课程名称"
-            variant="outlined"
-            density="compact"
-            rows="1"
-            max-rows="1"
-            style="margin: 2px"
-            v-model="course.name"
-          ></v-textarea>
-        </div>
-        <div style="display: flex; flex-direction: row; margin-top: 0px">
-          <v-textarea
-            label="授课教师"
-            variant="outlined"
-            density="compact"
-            rows="1"
-            max-rows="1"
-            max-width="300px"
-            v-model="course.teacher"
-            style="margin: 2px"
-          ></v-textarea>
-          <v-combobox
-            variant="outlined"
-            density="compact"
-            style="max-width: 300px; margin-left: 20px"
-            v-model="course.type"
-            :items="types"
-            label="课程类型"
-            multiple
-            chips
-          ></v-combobox>
-        </div>
-        <div style="display: flex; flex-direction: row; margin-top: 0px">
-          <v-combobox
-            variant="outlined"
-            density="compact"
-            style="max-width: 300px"
-            v-model="course.college"
-            :items="colleges"
-            label="开设学院"
-          ></v-combobox>
-          <v-combobox
-            variant="outlined"
-            density="compact"
-            style="max-width: 300px; margin-left: 20px"
-            v-model="course.campu"
-            :items="campus"
-            label="开设校区"
-          ></v-combobox>
-        </div>
-        <div style="display: flex; flex-direction: row-reverse; margin-top: 0px">
-          <v-btn
-            @click="submit"
-            variant="outlined"
-            style="height: 30px; margin-left: 20px"
-            color="#9c0c13"
-            >提交</v-btn
-          >
-          <v-btn
-            @click="cancel"
-            variant="outlined"
-            style="height: 30px; margin-left: 20px"
-            color="#9c0c13"
-            >取消</v-btn
-          >
-        </div>
+  <v-card style="width: 750px; padding: 10px">
+    <div style="display: flex; flex-direction: column">
+      <div style="display: flex; flex-direction: row">
+        <span style="font-size: 18px; font-weight: 600"> 编辑新课程 </span>
       </div>
-    </v-card>
-  </template>
-  
-  <script>
-    export default {
-      props:{
-        id:{
-          type:String,
-          default:'00000000',
-        }
+      <div style="display: flex; flex-direction: row; margin-top: 10px">
+        <v-textarea
+          label="课程名称"
+          variant="outlined"
+          density="compact"
+          rows="1"
+          max-rows="1"
+          style="margin: 2px"
+          v-model="course.name"
+        ></v-textarea>
+      </div>
+      <div style="display: flex; flex-direction: row; margin-top: 0px">
+        <v-textarea
+          label="授课教师"
+          variant="outlined"
+          density="compact"
+          rows="1"
+          max-rows="1"
+          max-width="300px"
+          v-model="course.teacher"
+          style="margin: 2px"
+        ></v-textarea>
+        <v-combobox
+          variant="outlined"
+          density="compact"
+          style="max-width: 300px; margin-left: 20px"
+          v-model="course.type"
+          :items="types"
+          label="课程类型"
+          multiple
+          chips
+        ></v-combobox>
+      </div>
+      <div style="display: flex; flex-direction: row; margin-top: 0px">
+        <v-combobox
+          variant="outlined"
+          density="compact"
+          style="max-width: 300px"
+          v-model="course.college"
+          :items="colleges"
+          label="开设学院"
+        ></v-combobox>
+        <v-combobox
+          variant="outlined"
+          density="compact"
+          style="max-width: 300px; margin-left: 20px"
+          v-model="course.campu"
+          :items="campus"
+          label="开设校区"
+        ></v-combobox>
+      </div>
+      <div style="display: flex; flex-direction: row; margin-top: 0px">
+        <v-combobox
+          variant="outlined"
+          density="compact"
+          style="max-width: 300px"
+          v-model="course.college"
+          :items="examineMethods"
+          label="考核方式"
+        ></v-combobox>
+        <v-combobox
+          variant="outlined"
+          density="compact"
+          style="max-width: 300px; margin-left: 20px"
+          v-model="course.teachMethod"
+          :items="teachMethods"
+          label="上课方式"
+        ></v-combobox>
+      </div>
+      <div style="display: flex; flex-direction: row-reverse; margin-top: 0px">
+        <v-btn
+          @click="submit"
+          variant="outlined"
+          style="height: 30px; margin-left: 20px"
+          color="#9c0c13"
+          >提交</v-btn
+        >
+        <v-btn
+          @click="cancel"
+          variant="outlined"
+          style="height: 30px; margin-left: 20px"
+          color="#9c0c13"
+          >取消</v-btn
+        >
+      </div>
+    </div>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    props: {
+      id: {
+        type: String,
+        default: '00000000',
       },
-      data() {
-        const colleges = [
-          '计算机科学与技术学院',
-          '智能学院',
-          '法学院',
-          '物理学院',
-          '网络安全学院',
-        ]
-        const campus = [
-          '青岛校区',
-          '威海校区',
-          '中心校区',
-          '软件园校区',
-          '兴隆山校区',
-          '洪家楼校区',
-          '千佛山校区',
-          '洪家楼校区',
-        ]
-        const types = [
-          '必修课',
-          '限选课',
-          '全校通选课',
-          '通识核心课',
-          '国学修养',
-          '创新创业',
-          '艺术审美',
-          '人文学科',
-          '自然科学',
-          '社会科学',
-          '工程技术',
-        ]
-        return {
-          course: {
-            name: '',
-            teacher: '',
-            college: '',
-            campu: '',
-            type: [],
-          },
-          colleges,
-          campus,
-          types,
-        }
-      },
-      methods: {
-        cancel() {
-          this.$emit('close')
+    },
+    data() {
+      const colleges = [
+        '计算机科学与技术学院',
+        '智能学院',
+        '法学院',
+        '物理学院',
+        '网络安全学院',
+        '其他',
+      ]
+      const campus = [
+        '青岛校区',
+        '威海校区',
+        '中心校区',
+        '软件园校区',
+        '兴隆山校区',
+        '洪家楼校区',
+        '千佛山校区',
+        '洪家楼校区',
+      ]
+      const types = [
+        '必修课',
+        '限选课',
+        '全校通选课',
+        '通识核心课',
+        '国学修养',
+        '创新创业',
+        '艺术审美',
+        '人文学科',
+        '自然科学',
+        '社会科学',
+        '工程技术',
+        '其他',
+      ]
+      const teachMethods = ['线上', '线下', '混合', '其他']
+      const examineMethods = ['考试', '论文', '项目展示', '其他']
+      return {
+        course: {
+          name: '',
+          teacher: '',
+          college: '',
+          campu: '',
+          teachMethod: '',
+          examineMethod: '',
+          type: [],
         },
-        submit() {
-          console.log(this.course)
-        },
+        colleges,
+        campus,
+        types,
+        teachMethods,
+        examineMethods,
+      }
+    },
+    methods: {
+      cancel() {
+        this.$emit('close')
       },
-    }
-  </script>
-  
+      submit() {
+        console.log(this.course)
+      },
+    },
+  }
+</script>
