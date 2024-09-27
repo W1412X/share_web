935e862f74d1ed55e29d55bd0b6b12e74aa8429c
W1412X <tor108@outlook.com>
2024-09-27 09:50:03 +0800

    添加了Markdown编辑器,增加Markdown编辑器的图片上传功能和Latex支持

--- src/main.js
+++ src/main.js
@@ -9,6 +9,43 @@ import * as directives from "vuetify/directives";
 import store from './store';
 import 'vuetify/dist/vuetify.min.css';
 import '@mdi/font/css/materialdesignicons.css';
+
+
+import VMdEditor from '@kangc/v-md-editor/lib/codemirror-editor';
+import '@kangc/v-md-editor/lib/style/codemirror-editor.css';
+import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
+import '@kangc/v-md-editor/lib/theme/style/github.css';
+// codemirror 编辑器的相关资源
+import Codemirror from 'codemirror';
+// mode
+import 'codemirror/mode/markdown/markdown';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/htmlmixed/htmlmixed';
+import 'codemirror/mode/vue/vue';
+// edit
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/edit/closetag';
+import 'codemirror/addon/edit/matchbrackets';
+// placeholder
+import 'codemirror/addon/display/placeholder';
+// active-line
+import 'codemirror/addon/selection/active-line';
+// scrollbar
+import 'codemirror/addon/scroll/simplescrollbars';
+import 'codemirror/addon/scroll/simplescrollbars.css';
+// style
+import 'codemirror/lib/codemirror.css';
+// highlightjs
+import hljs from 'highlight.js';
+import createKatexPlugin from '@kangc/v-md-editor/lib/plugins/katex/cdn';
+VMdEditor.Codemirror = Codemirror;
+VMdEditor.use(githubTheme, {
+  Hljs: hljs,
+})
+.use(createKatexPlugin())
+;
+
 const vuetify = createVuetify({
   components,
   directives,
@@ -18,4 +55,5 @@ app.provide(store);
 app.use(router)
   .use(vuetify)
   .use(store)
+  .use(VMdEditor)
   .mount('#app');
--- src/pages/MdEditorPage.vue
+++ src/pages/MdEditorPage.vue
@@ -7,7 +7,7 @@
               variant="outlined" auto-grow></sensitive-text-area>
           </div>
           <div style="width:100%;display: flex;justify-content: center;">
-            <markdown-upload-card @alert="alert"></markdown-upload-card>
+            <v-md-editor :disabled-menus="[]" @upload-image="handleUploadImage" v-model="article.content" height="500px"></v-md-editor>
           </div>
           <div style="width:100%;display: flex;justify-content: center;">
             <article-editor-bar @alert="alert"></article-editor-bar>
@@ -23,11 +23,9 @@
 </template>
 <script>
 import ArticleEditorBar from '@/components/ArticleEditorBar.vue';
-import MarkdownUploadCard from '@/components/MarkdownUploadCard.vue';
 import SensitiveTextArea from '@/components/SensitiveTextArea.vue';
 export default {
   components: {
-    MarkdownUploadCard,
     ArticleEditorBar,
     SensitiveTextArea,
   },
@@ -38,13 +36,42 @@ export default {
         color:'success',
         title:'',
         content:''
-      }
+      },
+      article: {
+          id:'00000000',
+          title: '标题',
+          authorName: '作者名称',
+          authorProfileUrl:'https://pic2.zhimg.com/v2-0dda71bc9ced142bf7bb2d6adbebe4f0_r.jpg?source=1940ef5c',
+          publishTime: '2024-09-01 00:00',
+          tags: ['计算机','测试'],
+          starCount: '0',
+          scanCount: '0',
+          replyCount:'0',
+          content: '',
+          type:'转载',
+          originLink:'https://sss',//如果文章类型为转载，反之为'',
+          sourceUrl:'https://sss.zip',//如果文章含有资源，反之为'',
+          sourceName:'这是资源',
+        },
     }
   },
   methods:{
     alert(msg){
       this.alertSet=msg;
-    }
+    },
+    handleUploadImage(event, insertImage, files) {
+      // 拿到 files 之后上传到文件服务器，然后向编辑框中插入对应的内容
+      console.log(files)
+      // 此处只做示例
+      insertImage({
+        url:
+          'https://tse4-mm.cn.bing.net/th/id/OIP-C.V_eRvXb8ynCpQ4lsAr65mgHaD6?w=312&h=180&c=7&r=0&o=5&dpr=1.2&pid=1.7',
+        desc: '在这里编辑图片描述',
+        width: 'auto',
+        height: 'auto',
+      });
+      this.alert({state:true,color:'success',title:'上传成功',content:'图片上传成功，现在您可以调整图片大小以及添加图片描述'})
+    },
   }
 }
 </script>
