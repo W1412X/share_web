e7a4aabe8d8dc4c509e422db08a928b0e5df612f
W1412X <tor108@outlook.com>
2024-09-28 12:44:22 +0800

    完善了首页的布局

--- src/pages/WelcomePage.vue
+++ src/pages/WelcomePage.vue
@@ -1,41 +1,97 @@
 <template>
-  <LoadingView v-model="progressMsg"></LoadingView>
-  <main fluid class="d-flex align-center justify-center" style="height: 100vh;">
-    <v-card class="text-center pa-5" outlined elevation="12" :style="{ backgroundColor: backgroundColor }">
-      <div class="mb-4">
-        <h1 class="display-1 font-weight-bold">这是一个大学学习助手</h1>
-        <h3 class="font-weight-light">旨在分享大学生活中的学习资源，以及提供一些常见问题的解答，课程选择的建议</h3>
-      </div>
-      <div style="margin: 20px;">
-        <v-btn prepend-icon="mdi-file-document" @click="toIntro" color="primary" class="mx-2">网站介绍</v-btn>
-      </div>
-      <div style="margin: 20px;">
-        <v-btn prepend-icon="mdi-github" @click="toAuthorPage(project)" color="primary" class="mx-2">项目仓库</v-btn>
-      </div>
-      <div>
-        <v-btn prepend-icon="mdi-github" @click="toAuthorPage(wx)" color="secondary" class="mx-2">W1412X</v-btn>
-        <v-btn prepend-icon="mdi-github" @click="toAuthorPage(wh)" color="secondary" class="mx-2">zzysssigm</v-btn>
-      </div>
-    </v-card>
-
-    <v-dialog v-model="dialog" max-width="290">
-      <v-card>
+  <v-dialog v-model="ifShowDialog" style="width: 100%;height:100%;justify-content: center;">
+    <div style="display: flex;justify-content: center;">
+      <v-card v-if="ifShowLogin" style="padding: 10px;width:200px">
         <v-card-title class="headline">敬请期待</v-card-title>
-        <v-card-text>这个功能正在开发中，敬请期待！</v-card-text>
+        <v-card-text>暂未对外开放</v-card-text>
         <v-card-actions>
-          <v-btn text @click="dialog = false">好的</v-btn>
+          <v-btn text @click="this.setLoginState(false)">好的</v-btn>
         </v-card-actions>
       </v-card>
-    </v-dialog>
+      <v-card v-if="ifShowContact" style="padding: 10px;width:450px">
+        <div style="font-size: 20px; font-weight: 600">联系我们</div>
+        <div style="display: flex; flex-direction: row">
+          <div style="display: flex; flex-direction: column; margin: 5px">
+            <div style="display: flex; flex-direction: row;margin-top: 20px;">
+              <span style="font-size: 16px; font-weight: 600;margin-bottom: 10px;">邮箱:
+                <a href="mailto:admin@sharesdu.com" style="color: #0074e8; font-weight: bold">admin@sharesdu.com</a>
+              </span>
+            </div>
+            <div style="display: flex; flex-direction: column;margin-bottom: 10px;">
+              <span style="font-size: 16px; font-weight: 600">QQ群:
+                <span>246680702</span>
+              </span>
+            </div>
+            <div style="display: flex; flex-direction: column"></div>
+          </div>
+          <div style="width: 150px;margin-left: 50px;">
+            <v-img src="/qq_img/group_qr.png"></v-img>
+          </div>
+        </div>
+        <v-card-actions>
+          <v-btn text @click="this.setContactState(false)">关闭</v-btn>
+        </v-card-actions>
+      </v-card>
+    </div>
+  </v-dialog>
+  <LoadingView v-model="progressMsg"></LoadingView>
+  <main fluid class="d-flex align-center justify-center;" style="height: 100vh;flex-direction: column;">
+    <div style="
+      width: 100%;
+      height: 50px;
+      border-bottom: 2px #9c0c13 solid;
+      display: flex;
+      flex-direction: row;
+    ">
+      <v-card-title style="color: rgba(156,12,19,0.9)">Share⛰️Sdu</v-card-title>
+      <v-spacer></v-spacer>
+      <v-btn height="100%" variant="plain" style="font-size: 16px; font-weight: 600">项目地址</v-btn>
+      <v-btn @click="toContact" height="100%" variant="plain" style="font-size: 16px; font-weight: 600">联系我们</v-btn>
+    </div>
+    <div style="display: flex;justify-content: center;align-items: center;">
+      <v-card class="text-center pa-5" outlined elevation="12" style="margin-top: 200px;">
+        <div class="mb-4">
+          <h1 class="display-1 font-weight-bold">这是一个大学学习助手</h1>
+          <h3 class="font-weight-light">旨在分享大学生活中的学习资源，以及提供一些常见问题的解答，课程选择的建议</h3>
+        </div>
+        <div style="margin: 20px;">
+          <v-btn prepend-icon="mdi-file-document" @click="toIntro" color="primary" class="mx-2">点击查看网站介绍</v-btn>
+        </div>
+        <div style="margin: 20px;">
+          <v-btn prepend-icon="mdi-login-variant" @click="toLogin" color="secondary" class="mx-2">注册成为一名用户</v-btn>
+        </div>
+      </v-card>
+    </div>
   </main>
-  <div style="width: 100%;display: flex;justify-content: center;margin-top: 10px;">
+  <div style="width: 100%;display: flex;justify-content: center;margin-top: 10px;background-color: #8a8a8a;">
     <div style="display: flex;flex-direction: row;font-size: 13px;font-weight: 600;"><a
-        href="https://beian.miit.gov.cn/" style="color: #8a8a8a;" target="_blank">鲁ICP备2024118409</a></div>
+        href="https://beian.miit.gov.cn/" style="color: #ffffff;" target="_blank">鲁ICP备2024118409</a></div>
   </div>
 </template>
 
 <script>
+import { ref, computed } from 'vue';
 export default {
+  setup() {
+    const ifShowLogin = ref(false);
+    const ifShowContact = ref(false);
+    const ifShowDialog = computed(() => {
+      return ifShowLogin.value || ifShowContact.value;
+    })
+    const setLoginState = (state) => {
+      ifShowLogin.value = state;
+    }
+    const setContactState = (state) => {
+      ifShowContact.value = state;
+    }
+    return {
+      ifShowContact,
+      ifShowDialog,
+      ifShowLogin,
+      setContactState,
+      setLoginState,
+    }
+  },
   data() {
     return {
       wx: 'https://github.com/W1412X',
@@ -51,14 +107,17 @@ export default {
     };
   },
   methods: {
-    toAuthorPage(url) {
-      window.open(url, '_blank');
+    toContact() {
+      this.setContactState(true);
     },
     toIntro() {
       this.$router.push({ name: 'IntroducePage' })
     },
-    enter() {
-      this.dialog = true; // 打开对话框
+    toLogin() {
+      this.setLoginState(true)
+    },
+    close() {
+      this.setContactState(false);
     }
   },
 }
